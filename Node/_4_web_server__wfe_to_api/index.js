a1da53a204a67fee7d219a717be238e715e1bb67
Roche-Olivier <rolivier@mmiholdings.co.za>
2019-06-22 16:48:31 +0200

    Added the WFE to API site code, cors not included
    Added the WFE to API site code, cors not included

--- Node/_4_web_server__wfe_to_api/index.js
+++ Node/_4_web_server__wfe_to_api/index.js
@@ -2,10 +2,17 @@ process.env.API_URI = 'http://localhost:3101'
 
 console.log("hello world")
 const express = require('express')
+const path = require('path')
 const app = express()
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
  
+app.use('/css/', express.static(path.join(__dirname, '/content/css')));
+app.use('/images/', express.static(path.join(__dirname, '/content/images')));
+app.use('/js/', express.static(path.join(__dirname, '/content/js')));
+
+app.get('/api-interact/', function (req, res) { res.sendFile(path.join(__dirname + '/content/pages/index.html')); });
+
 app.listen(3102)
