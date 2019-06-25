085a0b5803f9ef2c75d6d0e292e92ae14cda3ba6
Roche-Olivier <rolivier@mmiholdings.co.za>
2019-06-26 01:10:10 +0200

    Added API template
    Added API template

---
+++ Examples/Node/API_Template_2019_06/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use('/css/', express.static(path.join(__dirname, '/content/css')));
+app.use('/images/', express.static(path.join(__dirname, '/content/images')));
+app.use('/js/', express.static(path.join(__dirname, '/content/js')));
+app.use('/pages/', express.static(path.join(__dirname, '/content/pages')));
+
+app.get('/', function (req, res) { res.sendFile(path.join(__dirname + '/content/pages/index.html')); });
+app.get('/home/', function (req, res) { res.sendFile(path.join(__dirname + '/content/pages/index.html')); });
+
+app.listen(3100)
